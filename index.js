85935bc241e1cba711f2bee0b2cba48b7cb6b81f
hazycora <72580505+hazycora@users.noreply.github.com>
2020-11-17 09:14:36 +0000

    Update index.js

--- index.js
+++ index.js
@@ -29,8 +29,7 @@ client.on('message', message => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
 	
-	const command = client.commands.get(commandName);
-		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+	const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 	
 	if (!command) return;
 

9b68db7a8630d191bb87d7e30edd98c25cbc30d8
hazycora <72580505+hazycora@users.noreply.github.com>
2020-11-17 08:14:47 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,10 @@
 module.exports = {
 	name: 'ping',
 	description: 'Ping!',
-  usage: 'ping',
-  guildOnly: true,
-  cooldown: 5,
-  args: false,
+	usage: 'ping',
+	guildOnly: true,
+	cooldown: 5,
+	args: false,
 	execute(message, args) {
 		message.channel.send('Pong.');
 	},

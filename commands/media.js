ce27b40278f7312a79ea6bb7830dee55d39db555
hazycora <72580505+hazycora@users.noreply.github.com>
2020-11-19 05:56:48 +0000

    Update media.js

--- commands/media.js
+++ commands/media.js
@@ -11,65 +11,72 @@ module.exports = {
 	cooldown: 5,
 	args: true,
 	execute(message, args) {
-    let film = args.join(' '); 
-    film = film.trim();
-    //this is not actually needed, but I am adding it anyways in case OMDb ever forces people to use plus signs for requests.
-    let query = film.replace(/ /g, "plusSIGN");
-    query = encodeURIComponent(query);
-    query = query.replace(/plusSIGN/g, "+");
+		let film = args.join(' '); 
+		film = film.trim();
+		//this is not actually needed, but I am adding it anyways in case OMDb ever forces people to use plus signs for requests.
+		let query = film.replace(/ /g, "plusSIGN");
+		query = encodeURIComponent(query);
+		query = query.replace(/plusSIGN/g, "+");
 
-    fetch('http://www.omdbapi.com/?t='+query+'&apikey='+omdbToken)
-      .then(res => res.json())
-      .then((out) => {
-      let { Title, Plot, Actors, Director, Writer, Genre, Runtime, Ratings, Type, Production, Poster } = out;
-      let Time = "N/A";
-      if (Type=="series") {
-        Time = "Years: "+out.Years;
-      }else {
-        Time = "Years: "+out.Released;
-      }
-      let embedExtra;
-      if (Production==undefined) {
-        embedExtra = "Genre: "+Genre+"\nRuntime: "+Runtime+"\nYears: "+Time;
-      }else{
-        embedExtra = "Genre: "+Genre+"\nRuntime: "+Runtime+"\nYears: "+Time+"\nProduction: "+Production;
-      }
+		fetch('http://www.omdbapi.com/?t='+query+'&apikey='+omdbToken)
+			.then(res => res.json())
+			.then((out) => {
+			let { Title, Plot, Actors, Director, Writer, Genre, Runtime, Ratings, Type, Production, Poster } = out;
+			let Time = "N/A";
+			if (Type=="series") {
+				Time = "Years: "+out.Years;
+			}else {
+				Time = "Years: "+out.Released;
+			}
+			let embedExtra;
+			if (Production==undefined) {
+				embedExtra = "Genre: "+Genre+"\nRuntime: "+Runtime+"\nYears: "+Time;
+			}else{
+				embedExtra = "Genre: "+Genre+"\nRuntime: "+Runtime+"\nYears: "+Time+"\nProduction: "+Production;
+			}
+			
+			let embedRatings = "";
+			for (let i = 0; i < Ratings.length; i ++) {
+				embedRatings += "\n"+Ratings[i].Source+": "+Ratings[i].Value
+			}
+			embedRatings = embedRatings.replace("\n", ""); //remove first line break
 
-      const timeEmbed = {
-        "fields": [
-          {
-            "name": Title,
-            "value": Plot
-          },
-          {
-            "name": "Writers & Directors",
-            "value": "Director: "+Director+"\nWriter: "+Writer
-          },
-          {
-            "name": "Actors",
-            "value": Actors
-          },
-          {
-            "name": "Ratings",
-            "value": "Ratings currently unavailable."
-          },
-          {
-            "name": "Extra Info",
-            "value": embedExtra
-          }
-        ],
-        "author": {
-          "name": "Searching for \""+film+"\"",
-          "icon_url": "https://cdn.onlinewebfonts.com/svg/img_129504.png"
-        },
-        "thumbnail": {
-          "url": Poster
-        }
-      }
-      //end of that whole object thing
+			const timeEmbed = {
+				"color": embedColor,
+				"fields": [
+				  {
+				    "name": Title,
+				    "value": Plot
+				  },
+				  {
+				    "name": "Writers & Directors",
+				    "value": "Director: "+Director+"\nWriter: "+Writer
+				  },
+				  {
+				    "name": "Actors",
+				    "value": Actors
+				  },
+				  {
+				    "name": "Ratings",
+				    "value": "Ratings currently unavailable."
+				  },
+				  {
+				    "name": "Extra Info",
+				    "value": embedExtra
+				  }
+				],
+				"author": {
+				  "name": "Searching for \""+film+"\"",
+				  "icon_url": "https://cdn.onlinewebfonts.com/svg/img_129504.png"
+				},
+				"thumbnail": {
+				  "url": Poster
+				}
+			};
+			//end of that whole object thing
 
-      message.channel.send({ embed: timeEmbed });
-    }).catch(err => console.error(err));
-    
+			message.channel.send({ embed: timeEmbed });
+		}).catch(err => console.error(err));
+
 	},
 };
